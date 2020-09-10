d5b32a30f7fd96a0b819a6a2eabd28cb985173f6
Amitabh Das <amda@microsoft.com>
2020-09-10 09:52:45 +0530

    code added

---
+++ Scripts/Create-AppInsights.ps1
@@ -0,0 +1,75 @@
+<#
+.SYNOPSIS
+	This powershell script is going to deploy Application Insight.
+
+.DESCRIPTION
+    This script creates an App Insight by useing the ARM template.
+
+.OUTPUTS
+    Metadata Information
+
+.EXAMPLE
+    $Params = @{
+        ResourceGroupName = "Test-RG1"
+        AppInsightsName = "test-appIn"
+        appInsightsType = "web"
+    }
+
+    Create-AppInsights.ps1 @Params
+#>
+
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $true)]
+    [String] $ResourceGroupName,
+
+    [Parameter(Mandatory = $true)]
+    [ValidatePattern('^[a-z0-9\-]{3,15}$')]
+    [String] $AppInsightsName,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet('eastus2', 'westeurope', 'westus2')]
+    [String] $Location,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('web', 'java', 'other')]
+    [String] $AppInsightsType,
+
+    [Parameter(Mandatory = $false)]
+    [System.Object] $Tags
+)
+
+Write-Verbose "Creating parameters object for ARM Template."
+$ARMParams = @{
+    appInsightsName               = $AppInsightsName
+    location                      = $Location
+    appInsightsType               = $AppInsightsType
+    tags                           = $Tags
+}
+
+
+$TemplateName = 'applicationInsights.json'
+
+$TemplateFile = Join-Path -Path $PSScriptRoot -ChildPath "..\Templates\$TemplateName"
+
+Write-Host "Deploying ARM Template.. "
+$DeploymentName = (Get-ChildItem -Path $TemplateFile).BaseName + "-" + ((Get-Date).ToUniversalTime()).ToString("MMdd-HHmm")
+$DeploymentParams = @{
+    Name                    = $DeploymentName
+    ResourceGroupName       = $ResourceGroupName
+    TemplateFile            = $TemplateFile
+    TemplateParameterObject = $ARMParams
+    Force                   = $true
+    ErrorVariable           = "ErrorMessages"
+    ErrorAction             = "SilentlyContinue"
+}
+
+$Result = New-AzResourceGroupDeployment @DeploymentParams
+
+If ($ErrorMessages) {
+    Throw "Template deployment returned the following errors: $ErrorMessages"
+}
+else {
+    Write-Verbose "ApplicationInsights successfully created."
+    Write-Output $($Result.Outputs)
+}
